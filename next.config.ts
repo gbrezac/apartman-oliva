2e853331124d32c560f08dc1288f129a9652b5df
gbrezac <gbrezac@gmail.com>
2026-07-09 16:36:08 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
